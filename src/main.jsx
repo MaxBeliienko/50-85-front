2f985362fbab05c3a6fe8bc6d09a3a3e5e7fcc2d
MaxBeliienko <mbelienko@gmail.com>
2024-07-18 15:42:46 +0300

    Додані бібліотеки, створена попередня структура проекту, огорнутий App

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import 'modern-normalize';
+import App from './App.jsx';
+import './index.css';
+import { HelmetProvider } from 'react-helmet-async';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <BrowserRouter>
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
