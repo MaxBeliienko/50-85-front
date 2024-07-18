29589ada7223d215fc2e24976fcdd8d7e511f5ca
MaxBeliienko <mbelienko@gmail.com>
2024-07-18 17:58:28 +0300

    icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import Icon from './components/Icon';
 
 function App() {
-  return (
-    <>
-      <Icon Iconsvg width={'40'} height={'40'} iconName="eye" styles={'logo'} />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
