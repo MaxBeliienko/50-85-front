f58c919ac42300978b01c81f7512d84ee2a4b9be
MaxBeliienko <mbelienko@gmail.com>
2024-07-19 13:44:58 +0300

    edit

--- src/components/notFound/NotFound.jsx
+++ src/components/notFound/NotFound.jsx
@@ -2,7 +2,7 @@ import css from './NotFound.module.css';
 
 const NotFound = () => {
   return (
-    <div className="not-found">
+    <div className={css['not-found']}>
       <h1>404</h1>
       <p>Page not found</p>
     </div>
