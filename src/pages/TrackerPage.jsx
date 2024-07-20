645244028e05d7e61f4b43d41454c6480aa50d86
serg444s <143083342+serg444s@users.noreply.github.com>
2024-07-20 20:59:28 +0300

    Update TrackerPage.jsx

--- src/pages/TrackerPage.jsx
+++ src/pages/TrackerPage.jsx
@@ -1,16 +1,11 @@
 import css from './TrackerPage.module.css';
 import CalendarSection from '../components/CalendarSection/CalendarSection';
-import { useState } from 'react';
 
 const TrackerPage = () => {
-  const [dailyNorma, setDailyNorma] = useState(1);
-
-  const [waterQuantity, setWaterQuantity] = useState(0.5);
-
   return (
     <div className={css.container}>
       <h2>Tracker Page</h2>
-      <CalendarSection waterQuantity={(waterQuantity / dailyNorma) * 100} />
+      <CalendarSection waterQuantity={50} />
     </div>
   );
 };
