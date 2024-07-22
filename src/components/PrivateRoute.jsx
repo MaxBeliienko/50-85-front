d64916e6a1d65c55c8cde5f2606bc52768c88621
MaxBeliienko <mbelienko@gmail.com>
2024-07-22 09:58:53 +0300

    privat and restricted

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,11 @@
-// import { useSelector } from 'react-redux';
-// import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { selectIsLoggedIn } from '../redux/auth/selectors';
 
-// const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
-//   const isLoggedIn = useSelector(selectIsLoggedIn);
+const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
 
-//   return isLoggedIn ? Component : <Navigate to={redirectTo} />;
-// };
+  return isLoggedIn ? Component : <Navigate to={redirectTo} />;
+};
 
-// export default PrivateRoute;
+export default PrivateRoute;
