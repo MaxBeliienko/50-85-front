a854f50735ba3a5c7393c307a221d2741ade0f98
MaxBeliienko <mbelienko@gmail.com>
2024-07-23 21:57:54 +0300

    users in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from './auth/slice';
 import { waterReducer } from './water/slice';
+import { userReducer } from './user/slice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    users: userReducer,
     water: waterReducer,
   },
 });
