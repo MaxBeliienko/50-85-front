571fa01d486dac9f816e70d625c78155dbfbc3d3
MaxBeliienko <mbelienko@gmail.com>
2024-07-23 13:57:20 +0300

    edit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,18 +2,17 @@ import AdvantagesSection from '../components/AdvantagesSection/AdvantagesSection
 import WelcomeSection from '../components/WelcomeSection/WelcomeSection';
 import styles from '../stylesPages/HomePage.module.css';
 import { Audio } from 'react-loader-spinner';
-import { useSelector } from 'react-redux';
-import { selectLoading } from '../redux/water/selectors';
+// import { useSelector } from 'react-redux';
+// import { selectLoading } from '../redux/water/selectors';
 // import { useEffect } from 'react';
 // import { fetchDailyWater } from '../redux/water/operations';
-
 
 const HomePage = () => {
   return (
     <div className={styles.homePageWrapper}>
       <WelcomeSection />
       <AdvantagesSection />
-      {isLoading && (
+      {/* {selectLoading && (
         <Audio
           height="80"
           width="80"
@@ -23,7 +22,7 @@ const HomePage = () => {
           wrapperStyle
           wrapperClass
         />
-      )}
+      )} */}
     </div>
   );
 };
