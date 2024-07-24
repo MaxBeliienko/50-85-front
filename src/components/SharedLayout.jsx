b07b83fa9e493d4794816f82fe4c39dfea2fbd15
MaxBeliienko <mbelienko@gmail.com>
2024-07-24 14:43:05 +0300

    loader sharedLayout

--- src/components/SharedLayout.jsx
+++ src/components/SharedLayout.jsx
@@ -1,9 +1,33 @@
 import { Suspense } from 'react';
+import { Hearts } from 'react-loader-spinner';
 
 const SharedLayout = ({ children }) => {
   return (
     <div className="container">
-      <Suspense fallback={<p>Loading...</p>}>{children}</Suspense>
+      <Suspense
+        fallback={
+          <Hearts
+            height="300"
+            width="300"
+            radius="9"
+            color="green"
+            ariaLabel="loading"
+            wrapperStyle={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            wrapperClass="my-custom-class"
+            visible={true}
+            backgroundColor="#f0f0f0"
+            secondaryColor="#ff6347"
+            strokeWidth={3}
+            animationDuration={800}
+          />
+        }
+      >
+        {children}
+      </Suspense>
     </div>
   );
 };
