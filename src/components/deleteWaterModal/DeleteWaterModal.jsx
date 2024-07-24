31de973173bc131f3648d0f324d116c901cd3818
serg444s <143083342+serg444s@users.noreply.github.com>
2024-07-24 13:04:03 +0300

    upd modal on daily info
    модалки відкриваються і закриваються  при натисканні на відповідні кнопки, тости, update handleSubmit

--- src/components/deleteWaterModal/DeleteWaterModal.jsx
+++ src/components/deleteWaterModal/DeleteWaterModal.jsx
@@ -1,12 +1,26 @@
 import styles from './DeleteWaterModal.module.css';
+import { useDispatch } from 'react-redux';
+import { deleteWater } from '../../redux/water/operations';
+import toast from 'react-hot-toast';
 
-const DeleteWaterModal = ({ closeModal }) => {
+const DeleteWaterModal = ({ closeModal, id }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    console.log(id);
+    dispatch(deleteWater(id));
+    toast.success('Successfully delet');
+    closeModal();
+  };
+
   return (
     <div className={styles.modal}>
       <h6 className={styles.title}>Delete entry</h6>
       <p className={styles.text}>Are you sure you want to delete the entry?</p>
       <div className={styles.buttons}>
-        <button className={styles.confirmBtn}>Delete</button>
+        <button className={styles.confirmBtn} onClick={handleDelete}>
+          Delete
+        </button>
         <button className={styles.cancelBtn} onClick={closeModal}>
           Cancel
         </button>
--- src/components/waterModal/WaterModal.jsx
+++ src/components/waterModal/WaterModal.jsx
@@ -1,13 +1,27 @@
 import styles from './WaterModal.module.css';
 import WaterForm from '../waterForm/WaterForm';
+import { useDispatch } from 'react-redux';
+import { addWater, editWater } from '../../redux/water/operations';
+import toast from 'react-hot-toast';
 
-const WaterModal = ({ operationType, onAddWater }) => {
+const WaterModal = ({ operationType, onCloseModal }) => {
   let title;
+  const dispatch = useDispatch();
+
   if (operationType === 'add') title = 'Add water';
   if (operationType === 'edit') title = 'Edit the entered amount of water';
 
   const handleSubmit = data => {
-    onAddWater(data.volume / 1000);
+    if (operationType === 'add') {
+      dispatch(addWater(data.volume / 1000, data.time));
+      toast.success('Successfully add');
+      onCloseModal();
+    }
+    if (operationType === 'edit') {
+      dispatch(editWater(data.volume / 1000, data.time));
+      toast.success('Successfully edit');
+      onCloseModal();
+    }
   };
 
   return (
