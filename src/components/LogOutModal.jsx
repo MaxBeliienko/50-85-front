839330da03cee20ddffb30aad4309f16a7f9dfd5
HannaPalianytsia <hannapalianytsia1@gmail.com>
2024-07-24 13:35:13 +0300

    є логаут

--- src/components/LogOutModal.jsx
+++ src/components/LogOutModal.jsx
@@ -1,12 +1,21 @@
+import { useDispatch } from 'react-redux';
 import styles from './deleteWaterModal/DeleteWaterModal.module.css';
+import { logOut } from '../redux/auth/operations';
 
 const LogOutModal = ({ closeModal }) => {
+  const dispatch = useDispatch();
+
   return (
     <div className={styles.modal}>
       <h6 className={styles.title}>Log out</h6>
       <p className={styles.text}>Do you really want to leave?</p>
       <div className={styles.buttons}>
-        <button className={styles.confirmBtn}>Log out</button>
+        <button
+          className={styles.confirmBtn}
+          onClick={() => dispatch(logOut())}
+        >
+          Log out
+        </button>
         <button className={styles.cancelBtn} onClick={closeModal}>
           Cancel
         </button>
