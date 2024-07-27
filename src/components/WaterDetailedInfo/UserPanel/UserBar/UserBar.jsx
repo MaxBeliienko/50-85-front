5d3736c720c2e979b83a022e1c4faf4717287071
OleksiiP <alex1204park@gmail.com>
2024-07-27 20:25:50 +0300

    fix main user menu

--- src/components/WaterDetailedInfo/UserPanel/UserBar/UserBar.jsx
+++ src/components/WaterDetailedInfo/UserPanel/UserBar/UserBar.jsx
@@ -58,23 +58,27 @@ const UserBar = () => {
       <div className={css.userButtonContainer} data-popup="true">
         <button className={css.userButton} onClick={togglePopup}>
           <div className={css.buttonContainer}>
-            <span className={css.userName}>{userBar.name || 'User'}</span>
+            <span className={css.userName}>
+              {userBar.name || 'User'}
+            </span>
 
-            <img
-              src={
-                userBar.avatar ||
-                'https://cdn-icons-png.flaticon.com/512/6596/6596121.png'
-              }
-              className={css.userPhoto}
-            />
-
-            <div className={css[arrowClass]}>
-              <Iconsvg
-                width={10}
-                height={6}
-                iconName="vector"
-                styles={css.svg}
+            <div className={css.userWpapper}>
+              <img
+                src={
+                  userBar.avatar ||
+                  'https://cdn-icons-png.flaticon.com/512/6596/6596121.png'
+                }
+                className={css.userPhoto}
               />
+
+              <div className={css[arrowClass]}>
+                <Iconsvg
+                  width={10}
+                  height={6}
+                  iconName="vector"
+                  styles={css.svg}
+                />
+              </div>
             </div>
           </div>
         </button>
