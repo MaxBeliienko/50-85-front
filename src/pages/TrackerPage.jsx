71b3394f6fbe90a8be86308dbf3ff2627517fdcd
MaxBeliienko <mbelienko@gmail.com>
2024-07-28 12:07:50 +0300

    запит на юзера з App в Tracker

--- src/pages/TrackerPage.jsx
+++ src/pages/TrackerPage.jsx
@@ -1,8 +1,16 @@
 import css from './TrackerPage.module.css';
+import { useEffect } from 'react';
 import MainWaterInfo from '../components/WatterMainInfo/MainWaterInfo/MainWaterInfo';
 import WaterDetailedInfo from '../components/WaterDetailedInfo/UserPanel/WaterDetailedInfo';
+import { useDispatch } from 'react-redux';
+import { getUserProfile } from '../redux/user/operations';
 
 const TrackerPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUserProfile());
+  }, [dispatch]);
   return (
     <div className={css.container}>
       <MainWaterInfo />
