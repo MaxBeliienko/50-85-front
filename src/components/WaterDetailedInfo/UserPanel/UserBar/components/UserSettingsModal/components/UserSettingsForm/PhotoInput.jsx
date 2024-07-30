a3c4c2a5b9b38e50eee2e3b2fc988f434d67f4e4
serg444s <143083342+serg444s@users.noreply.github.com>
2024-07-30 12:04:11 +0300

    upd
    хай пока так буде

---
+++ src/components/WaterDetailedInfo/UserPanel/UserBar/components/UserSettingsModal/components/UserSettingsForm/PhotoInput.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import css from './PhotoInput.module.css';
+import defaultAvatar from '../../../../../../../../../public/customers/defaultAvatar.png';
+import Iconsvg from '../../../../../../../Icon';
+
+const PhotoInput = ({ user, setSelectedFile, register }) => {
+  const [photoUrl, setPhotoUrl] = useState(user.photo || defaultAvatar);
+
+  const handlePhotoChange = event => {
+    const file = event.target.files[0];
+    if (file) {
+      setPhotoUrl(URL.createObjectURL(file));
+      setSelectedFile(file);
+    }
+  };
+
+  return (
+    <div className={css.container}>
+      <div className={css.wrapper}>
+        <img className={css.img} src={photoUrl} alt="photo"></img>
+      </div>
+      <label className={css.label} htmlFor={'photo'}>
+        <Iconsvg width={21} height={20} iconName={'log-out'} styles={css.svg} />
+        Upload a photo
+      </label>
+      <input
+        className={css.input}
+        id={'photo'}
+        type="file"
+        {...register('photo')}
+        onChange={handlePhotoChange}
+      />
+      {/* {errors.photo && <p className={css.errorText}>{css.photo.message}</p>} */}
+    </div>
+  );
+};
+
+export default PhotoInput;
